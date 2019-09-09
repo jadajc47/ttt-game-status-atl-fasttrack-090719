1f81cb3ece690c38b6e34cc55896e78caf79b39f
friendly-mainframe-3495 <culpepper.jada@gmail.com>
2019-09-09 22:20:42 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -34,40 +34,60 @@ if position_1 == "X" && position_2 == "X" && position_3 == "X"
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combo
     end
-  }
   return false
 end
 
 def full?(board)
-  board.all? {|index| index == "X" || index == "O"}
+  if board.include?(' ') || board.include?('')
+    return false
+else
+    return true
+  end
 end
 
+#IS THERE A DRAW?
 def draw?(board)
   if !won?(board) && full?(board)
-    return true
-  else
     return false
   end
 end
 
+#HAS SOMETHING HAPPENED?
 def over?(board)
-  if won?(board) || full?(board) || draw?(board)
+  puts 'is it over?'
+  if won?(board) || draw?(board) || full?(board)
     return true
   else
+    puts 'no keep going'
     return false
   end
 end
 
-def winner (board)
-  index = []
-  index = won?(board)
-  if index == false
+#WHO WON?
+def winner(board)
+  if !won?(board)
     return nil
-  else
-    if board[index[0]] == "X"
-      return "X"
-    else
-      return "O"
+  else WIN_COMBINATIONS.each do |win_combo|
+    if check_win_combination?(board, 'X', win_combo)
+      return 'X'
+    elsif check_win_combination?(board, 'O', win_combo)
+      return 'O'
     end
+  end
+end
+end
+
+
+def play(board)
+  until over?(board) == true || won?(board) != false
+  puts 'turn'
+    turn(board)
+  end
+  if winner(board)
+    puts "Congratulations!"
+  elsif draw?(board)
+    puts "Draw!"
+  else
+    return nil
   end
 end
